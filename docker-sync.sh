8b8929a60e498549d5e66ce0b4ab6556205e3119
weichen <lokisw@qq.com>
2026-03-23 09:04:03 +0800

    sdfsdf

--- docker-sync.sh
+++ docker-sync.sh
@@ -8,6 +8,7 @@ git checkout -f .
 git reset --hard origin/master
 
 echo ">>> 构建项目..."
+export DATABASE_URL="postgres://postgres:123456@172.17.0.1:5432/abt"
 cargo build --release -p abt-grpc
 
 echo ">>> 构建完成！"

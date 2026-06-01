525d2f1c5fcdf71f69bdd97af32236b5798747c6
weichen <lokisw@qq.com>
2026-06-01 16:43:01 +0800

    chore(docker): switch deployment branch from master to old

--- tdocker/docker-sync.sh
+++ tdocker/docker-sync.sh
@@ -7,7 +7,7 @@ export RUSTUP_DIST_SERVER="https://rsproxy.cn"
 export RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"
 git fetch origin
 git checkout -f .
-git reset --hard origin/master
+git reset --hard origin/old
 
 echo ">>> 构建项目..."
 export DATABASE_URL="postgres://postgres:123456@172.17.0.1:5432/abt"
